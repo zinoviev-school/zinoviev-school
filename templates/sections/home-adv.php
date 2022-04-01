6a92363ec2733c4be3bab12c43dbb8886b9f9406
inna-Z-website <inna@z-website.ru>
2022-04-01 17:31:10 +0300

    block advantages

---
+++ templates/sections/home-adv.php
@@ -0,0 +1,49 @@
+<div class="container">
+		<div class="section-title">			
+		<h2 class="bg-title-section bg-primary px-3 py-2 mb-5"><?php esc_html_e( 'Why trust us', 'zinoviev-school' ); ?></h2>		
+		</div>
+		<div class="row">
+	<div class="col-md-6 col-lg-4 mb-5 mb-lg-0">
+		<div class="adv-item h-100">
+			<div class="haed-item bg-white text-center">
+				<img class="img-fluid" src="/wp-content/themes/zinoviev-school/src/img/swimming-people.svg" alt="swimming" loading="lazy">
+		</div>
+			<div class="body-item d-flex flex-column h-100">
+				<h3 class="adv-item-title mb-4 text-uppercase"><?php echo nl2br(esc_html__( "Individual\ntraining", 'zinoviev-school' )); ?></h3>
+				<p class="text-adv mb-4 flex-grow-1"><?php esc_html_e( 'Allows you to use the features and abilities of the student and contributes to the rapid learning and achievement of results.', 'zinoviev-school' ); ?></p>
+				<div class="footer-item-img text-center">
+					<img class="img-fluid swim-logo-people" src="/wp-content/themes/zinoviev-school/src/img/people.svg" alt="people" loading="lazy" />
+			</div>
+			</div>
+		</div>
+	</div>
+	<div class="col-md-6 col-lg-4 mb-5 mb-lg-0">
+		<div class="adv-item h-100">
+			<div class="haed-item bg-white text-center">
+				<img class="img-fluid" src="/wp-content/themes/zinoviev-school/src/img/swimming-people.svg" alt="swimming" loading="lazy">
+		</div>
+			<div class="body-item d-flex flex-column h-100">
+				<h3 class="adv-item-title mb-4 text-uppercase"><?php echo nl2br(esc_html__( "Author's\ntechnique", 'zinoviev-school' )); ?></h3>
+				<p class="text-adv mb-4 flex-grow-1"><?php esc_html_e( 'We took the best practices of domestic and foreign swimming training. We are not looking for champions, we are creating them.', 'zinoviev-school' ); ?></p>
+				<div class="footer-item-img text-center">
+					<img class="img-fluid swim-logo-people" src="/wp-content/themes/zinoviev-school/src/img/people.svg" alt="people" loading="lazy" />
+			</div>
+			</div>
+		</div>
+	</div>
+	<div class="col-md-6 col-lg-4 mb-5 mb-lg-0">
+		<div class="adv-item h-100">
+			<div class="haed-item bg-white text-center">
+				<img class="img-fluid" src="/wp-content/themes/zinoviev-school/src/img/swimming-people.svg" alt="swimming" loading="lazy">
+		</div>
+			<div class="body-item d-flex flex-column h-100">
+				<h3 class="adv-item-title mb-4 text-uppercase"><?php echo nl2br(esc_html__( "Team of\nprofessionals", 'zinoviev-school' )); ?></h3>
+				<p class="text-adv mb-4 flex-grow-1"><?php esc_html_e( 'Our trainers are swimmers with proven skills, specialized education and extensive work experience.', 'zinoviev-school' ); ?></p>
+				<div class="footer-item-img text-center">
+					<img class="img-fluid swim-logo-people" src="/wp-content/themes/zinoviev-school/src/img/people.svg" alt="people" loading="lazy" />
+			</div>
+			</div>
+		</div>
+	</div>
+	</div>
+</div>
