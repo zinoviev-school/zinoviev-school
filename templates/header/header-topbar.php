3365eff853aedaec8eaf341ea41c86fd209303ab
”Denis <info@codeweber.com>
2022-04-14 23:02:48 +0300

    header v1

--- templates/header/header-topbar.php
+++ templates/header/header-topbar.php
@@ -1,13 +1,8 @@
-<div id="header-topbar" class="navbar navbar-expand navbar-dark">  
-	  
-	<div class="container justify-content-center">
-
+<div id="header-topbar" class="navbar navbar-expand navbar-dark">
+	<div class="container justify-content-between">
 		<?php
-
-		get_template_part( 'templates/components/socialicons', '' );
-
+		get_template_part('templates/components/socialicons', '');
 		?>
-		
+		<a href="tel:<?php the_field('contacts_phone', 'option'); ?> "><i class="fa-solid fa-phone"></i><?php the_field('contacts_phone', 'option'); ?></a>
 	</div>
-
 </div>
